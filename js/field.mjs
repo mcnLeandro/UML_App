1e890a6904df5f963c2a278f0991f9255874ca81
Leandro <leandro83g@icloud.com>
2021-04-26 16:53:24 +0900

    refactoring and adding gitignore

--- js/field.mjs
+++ js/field.mjs
@@ -1,10 +1,17 @@
 let canvas = document.querySelector('canvas');
+let svg = document.querySelector('svg')
+let g = document.querySelector('svg').querySelector('g');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
+
+let UMLClassStrokerColor = 'rgb(185, 198, 209)';
+let lineWidthDafault = 1;
+
 let data = []
+
 let colorArray = [
     '#15C2A6',
     '#353535',
@@ -13,14 +20,44 @@ let colorArray = [
     '#CB2624'
 ]
 
+function distance(x1, y1, x2, y2){
+    const xDist = x2 - x1;
+    const yDist = y2 - y1;
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
+
+
+
+
 // ===============================================
 // field
 // ===============================================
 
 let field = {
+
+    objects:[],
+
     x:0,
     y:0,
 
+    color:'rgba(238,242,246)',
+
+    draw: function(){
+
+        //reset field setting
+        c.fillStyle = this.color;
+        c.lineWidth = lineWidthDafault;
+
+        //set field style
+        c.fillRect(-field.x, -field.y, innerWidth, innerHeight)
+        this.drawGrid(100);
+
+        // console.log(this.x + " " + field.x)
+        //reinit
+        this.objects.forEach(e => e.update());
+
+    },
     updateAsCanvas : function(x,y){
 
         c.clearRect(-this.x,-this.y,innerWidth,innerHeight)
@@ -28,36 +65,38 @@ let field = {
         this.x = this.x + x;
         this.y = this.y + y;
 
-        console.log(`x : ${this.x}, Y : ${this.y}`)
+        // console.log(`x : ${this.x}, Y : ${this.y}`)
 
         c.translate(x, y)
-        this.drawGrid(100);
+        g.style.transform = `translate(${this.x+10}px,${this.y+10}px)`
         
-        //reinit
-        data.forEach(e => e.update());
+        this.draw() 
+       
     },
     updateAsView : function(x,y){
 
         c.clearRect(-this.x, -this.y, innerWidth, innerHeight)
         c.translate(-this.x, -this.y)
+
         
         this.x = x;
         this.y = y;
-
+        
         c.translate(x, y)
-        this.drawGrid(100);
-
-        ///reinit
-        data.forEach(e => e.update());
+        // g.style.transform = `translate(${x+10}px,${y+10}px)` 
+        g.style.transform = `translate(${x+10}px,${y+10}px)` 
+        
+        this.draw()
     },
     drawGrid: (gap)=>{
 
+        // if(gap == 100)return;
 
         let w = innerWidth-field.x;
         let h = innerHeight-field.y;
     
         c.beginPath();
-    
+        c.globalAlpha = 0.5
     
         // +x
         for(let x = 0 ; x < w; x = x+gap ){
@@ -91,105 +130,20 @@ let field = {
         c.strokeStyle = 'black'
         c.stroke();
         c.closePath();
+        c.globalAlpha = 1
+    
+    },
+    init:function(){
+
+        this.objects.push(new Circle(0, 0, 10, 10, 10));
+        this.objects.push(new Circle(100, 100, 10, 10, 10));
+        this.objects.push(new Circle(-100, -100, 10, 10, 10));
+        
+        field.updateAsView(500, 500)
     
     }
 
 }
-
-
-// user Interface status
-
-let mouse = {
-    x:undefined,
-    y:undefined,
-    lastDown:{
-        x:undefined,
-        y:undefined,
-        // Is it really suit to here, inside of mouse? yes or no, and why?
-        diffFromField:{
-            x: undefined,
-            y: undefined
-        }
-    },
-    // is this really need ? but I thought lastDown and click is different. click is propablly same as mouseup, or little bit different
-    click:{
-        x:undefined,
-        y:undefined,
-    },
-    isClicked:false
-}
-
-// feature implemention of scroll and move field
-// let wheel = {
-//     x:Math.floor(innerWidth/2),
-//     y:Math.floor(innerHeight/2)
-// }
-
-window.addEventListener('resize', ()=>{
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    // init()
-
-})
-window.addEventListener('mousemove', e => {
-
-    mouse.x = e.x;
-    mouse.y = e.y;
-    // console.log(`x : ${mouse.x}, Y : ${mouse.y}`)
-
-})
-window.addEventListener("mousedown",e => {
-
-    mouse.isClicked = true;
-    mouse.lastDown.x = e.x;
-    mouse.lastDown.y = e.y;
-    mouse.lastDown.diffFromField.x = e.x-field.x;
-    mouse.lastDown.diffFromField.y = e.y-field.y;
-
-})
-window.addEventListener("mouseup",e => {
-
-    mouse.isClicked = false;
-
-})
-window.addEventListener('wheel',e => {
-
-    wheel.x = e.x;
-    wheel.y = e.y;
-
-})
-window.addEventListener('click',e => {
-
-    mouse.click.x = e.x;
-    mouse.click.y = e.y;
-
-})
-
-
-/// grab field event
-
-window.addEventListener('keydown',e => {
-
-    if(e.key == ' ' && mouse.isClicked){
-
-        field.updateAsView(mouse.x - mouse.lastDown.diffFromField.x, mouse.y - mouse.lastDown.diffFromField.y);
-        
-    } 
-
-})
-window.addEventListener('keydown',e => {
-
-    if(e.key == ' ')canvas.style.cursor =  "grab"; 
-    if(e.key == ' ' && mouse.isClicked) canvas.style.cursor = "grabbing";
-    // ERROR: keydown space and mousedown, then keyup space before mouseup. cursur swaps default to hand.
-    window.addEventListener('mouseup',()=> canvas.style.cursor = "grab")
-    window.addEventListener('keyup' , ()=> canvas.style.cursor = 'default')
-
-})
-
-
-
 
 class Circle {
     constructor(x, y, dx, dy, radius) {
@@ -221,15 +175,125 @@ class Circle {
     }
 }
 
-function init(){
 
-    data.push(new Circle(0, 0, 10, 10, 10));
-    data.push(new Circle(100, 100, 10, 10, 10));
-    data.push(new Circle(-100, -100, 10, 10, 10));
+// ===============================================
+// mouse status
+// ===============================================
+
+let mouse = {
+    x:undefined,
+    y:undefined,
     
-    field.updateAsView(500, 500)
+    lastDown:{
+        x:undefined,
+        y:undefined,
+    },
+    
+    // is this really need ? but I thought lastDown and click is different. click is propablly same as mouseup, or little bit different
+    click:{
+        x:undefined,
+        y:undefined,
+    },
+    isClicked:false,
 
+    field:{
+        x:undefined,
+        y:undefined,
+        lastDown:{
+            x:undefined,
+            y:undefined,
+        },
+    }
 }
 
-init()
+// feature implemention of scroll and move field
+// let wheel = {
+//     x:Math.floor(innerWidth/2),
+//     y:Math.floor(innerHeight/2)
+// }
 
+window.addEventListener('resize', ()=>{
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    // init()
+
+})
+window.addEventListener('mousemove', e => {
+
+    mouse.x = e.x;
+    mouse.y = e.y;
+    mouse.field.x = e.x - field.x;
+    mouse.field.y = e.y - field.y;
+    // console.log(`x : ${mouse.x}, Y : ${mouse.y}`)
+
+})
+window.addEventListener("mousedown",e => {
+
+    mouse.isClicked = true;
+    mouse.lastDown.x = e.x;
+    mouse.lastDown.y = e.y;
+    mouse.field.lastDown.x = e.x-field.x;
+    mouse.field.lastDown.y = e.y-field.y;
+
+})
+window.addEventListener("mouseup",e => {
+
+    mouse.isClicked = false;
+
+})
+// window.addEventListener('wheel',e => {
+
+//     wheel.x = e.x;
+//     wheel.y = e.y;
+
+// })
+window.addEventListener('click',e => {
+
+    mouse.click.x = e.x;
+    mouse.click.y = e.y;
+    console.log(`x: ${e.x},y: ${e.y}`)
+
+})
+
+
+// ===============================================
+// grab listenner
+// ===============================================
+
+window.addEventListener('keydown',e => {
+
+    if(e.key == ' ' && mouse.isClicked){
+
+        field.updateAsView(mouse.x - mouse.field.lastDown.x, mouse.y - mouse.field.lastDown.y);
+        
+    } 
+
+})
+window.addEventListener('keydown',e => {
+
+    if(e.key == ' '){
+        canvas.style.cursor =  "grab";
+        svg.style.cursor =  "grab";
+    } 
+
+    if(e.key == ' ' && mouse.isClicked){
+        canvas.style.cursor = "grabbing";
+        svg.style.cursor = "grabbing";
+    } 
+
+    window.addEventListener('mouseup',()=>{
+        if(mouse.isClicked){
+            canvas.style.cursor = "grab"
+            svg.style.cursor = "grab"
+        }
+    })
+    window.addEventListener('keyup' , ()=> {
+        canvas.style.cursor = 'default'
+        svg.style.cursor = 'default'
+    })
+
+})
+
+
+field.init()
