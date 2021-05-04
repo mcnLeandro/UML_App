31fd1ef4b40a810b3569aa2b5bf755c017d13438
Leandro <leandro83g@icloud.com>
2021-05-04 14:25:07 +0900

    use webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import './../css/style.css'
+
 import {field} from "./modules/field.js"
+
 
 field.init()
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+const config = {
+    mode: "development",
+    context: path.resolve(__dirname, 'src'),
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/js/modules'),
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                include: path.resolve(__dirname, 'src/css'),
+            },
+            {
+                // Babel 用のローダー
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                            ]
+                        }
+                    }
+                ],
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 3000,
+    },
+};
+
+module.exports = config;
