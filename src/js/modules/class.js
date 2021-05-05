fd7d2febd4c1efdb2daaddb0a1d96349a976584e
Leandro <leandro83g@icloud.com>
2021-05-05 20:36:37 +0900

    add class file, add TODO

---
+++ src/js/modules/class.js
@@ -0,0 +1,15 @@
+
+// TODO: create Class class
+// TODO: create Section class ( another branch)
+// TODO: create Column class (another branch)
+
+// TODO: drag
+// TODO: focus (svg should be implemented in another branch)
+
+// TODO: edit menu (svg)
+// TODO: edit status (svg)
+
+
+// TODO: resize (just a method or something)
+// TODO: resize (with svg)
+
