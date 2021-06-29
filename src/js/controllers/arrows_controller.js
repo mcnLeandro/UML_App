e4aeae824f5cccd4545ce41808ad750bf024495f
Leandro <leandro83g@icloud.com>
2021-06-29 09:47:21 +0900

    :rotating_light: Output Test of Path

--- src/js/controllers/arrows_controller.js
+++ src/js/controllers/arrows_controller.js
@@ -1,5 +1,12 @@
 import { UMLObjectsController } from "js/controllers/uml_objects_controller"
+import { Arrow } from "js/models/arrow"
 
 export class ArrowsController extends UMLObjectsController {
-    
+
+    static create(){
+        return new Arrow()
+    }
+    static update(arrow){
+
+    }
 }
--- src/js/models/arrow.js
+++ src/js/models/arrow.js
@@ -1,9 +1,17 @@
-import { UMLObject } from "js/uml_object";
+import { UMLObject } from "js/models/uml_object";
+
+
 
 export class Arrow extends UMLObject {
 
     constructor(){
-        
+        super()
+        this.path = new Path()
+        this.path.strokeColor = 'black'
+        this.path.add(new Point(20, 20));
+        this.path.add(new Point(80, 80));
+        this.path.add(new Point(100, 100));
+        this.path.add(new Point(8, 200));
     }
 
 }
