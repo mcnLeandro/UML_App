28c50e0d423581d0e58d67c32ee2952caa1c9b08
Leandro <leandro83g@icloud.com>
2021-08-06 14:45:27 +0900

    # Fix bug of dividers_listener
    - Divider doesn't need to focus, so made impossible

--- src/js/listeners/dividers_listener.js
+++ src/js/listeners/dividers_listener.js
@@ -2,14 +2,12 @@ import { UMLObjectsListener } from "js/listeners/uml_objects_listener";
 
 export class DividersListener extends UMLObjectsListener{
 
-    /*
     static set(umlObj){
 
-        super.set(umlObj)
+        // super.set(umlObj)
 
     }
-    */
-    /*     
+    /* 
     static onClickToFocus(umlObj){
 
         super.onClickToFocus(umlObj)
